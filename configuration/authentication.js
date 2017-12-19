5e41f6e3e2fcb9b535e698c44161dc4de6dd66aa
anurag <sharmaanurag.236@gmail.com>
2017-12-20 02:03:24 +0530

    minor

--- configuration/authentication.js
+++ configuration/authentication.js
@@ -1,7 +1,7 @@
 module.exports = {
     'facebookAuth': {
-        'clientID': 'abcd',
-        'clientSecret': 'efgh',
-        'callbackURL': 'xyz'
+        'clientID': '300021240505791',
+        'clientSecret': 'a6c7e4627983bc97f7c5fcc47dd2eb8a',
+        'callbackURL': 'http://ec2-13-127-92-66.ap-south-1.compute.amazonaws.com/authentication/facebook/callback'
     }
 };
